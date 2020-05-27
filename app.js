a77198eb1c74f10cdaa638acf47356ab0b0a7321
RobinPalmier <robinpalmier98@gmail.com>
2020-05-27 17:33:05 +0200

    ajout de routes api pour fetch les data depuis l'api

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const schedule = require('node-schedule');
 
 // Functions
 const Twitter = require('./functions/twitterFunctions');
@@ -24,27 +23,7 @@ mongoose.connect(
 )
 
 // Twiter fetch data :
-Twitter.twitterFetchData();
-
-const job = schedule.scheduleJob('/10 * * * * *', () => {
-    console.log('execute job');
-    let word = 'tpmp';
-
-    T.get('trends/place', { id: 615702 })
-    .catch(function (err) {
-        console.log('caught error', err.stack);
-    })
-    .then(function (result) {
-        // console.log(JSON.stringify(result.data, null, 2));
-        // insérer ou mettre à jour tous les mots clés tendance (50)
-        // Attention, l'api retourne parfois un tweet_volume null, ignorer
-
-        for (let keyword of result.data[0].trends) {
-            console.log("keyword : " + keyword.name + " tweets : " + keyword.tweet_volume);
-        }
-
-    }); 
-});
+Twitter.twitterFetchDataByCountry(615702);
 
 // Create app Express :
 const app = express()
--- functions/twitterFunctions.js
+++ functions/twitterFunctions.js
@@ -1,21 +1,52 @@
-const accesTwiter = require('../twitter-connect');
+const accessTwiter = require('../twitter-connect');
+const CronJob = require('cron').CronJob;
 
 const TwitterFunctions = {
-    twitterFetchData: () => {
-        let count = 2;
-        let params = {q: 'node.js', count};
-        accesTwiter.get('search/tweets',params, function(error, data, response) {
-            if(error) return console.log("Erreur : ", error)
-            if(response.statusCode != 200) return console.log("Erreur de connexion à l'API Twitter.");
-    
-            console.log(data.statuses);
-    
-            // const map1 = data.statuses.map(idTweet => idTweet.id); // Convertir en filter
-            // console.log(map1);
-    
+    keywordsParse: (keyword) => {
+        let regex = /[#]/g;
+        let isMatch = keyword.match(regex);
+        return !isMatch ? "#".concat(keyword) : keyword;
+    },
+    twitterFetchDataByCountry: (idCountry) => {
+        new CronJob('*/10 * * * * *', () => {
+            accessTwiter.get('trends/place', { id: idCountry, count: 2 })
+            .catch((err) => {
+                console.log('ERROR :', err.stack);
+            })
+            .then((result) => {
+                let parseData = [];
+                let date = result[0].created_at;
+
+                result[0].trends.map((keyword) => {
+                    parseData.push({
+                        date: date,
+                        count: keyword.tweet_volume,
+                        keyword: keyword.name
+                    })
+                });
+                console.log('data parsed', parseData);
             });
-    
-        setTimeout(TwitterFunctions.twitterFetchData, 600000);
+        }).start();
+    },
+    twitterFetchDataByKeywords: (word) => {
+        new CronJob('*/10 * * * *', () => {
+            accessTwiter.get('search/tweets', { q: TwitterFunctions.keywordsParse(word), count: 2 })
+            .catch((err) => {
+                console.log('ERROR :', err.stack);
+            })
+            .then(function (result) {
+                let parseData = [];
+                result.statuses.map((keyword) => { 
+                    parseData.push({
+                        date: keyword.created_at,
+                        idTweet: keyword.id,
+                        keyword: TwitterFunctions.keywordsParse(word)
+                    })
+                });
+
+                console.log('data parsed', parseData);
+            });
+        }).start();
     }
 }
 
