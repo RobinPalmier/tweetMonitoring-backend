b73386f81a2ccf5e6361b030092b289a20485ec1
RobinPalmier <robinpalmier98@gmail.com>
2020-05-27 15:01:03 +0200

    impasse

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const schedule = require('node-schedule');
 
 // Functions
 const Twitter = require('./functions/twitterFunctions');
@@ -15,12 +16,35 @@ dotenv.config();
 // Database connexion
 mongoose.connect(
     process.env.DB_CONNECT,
-    { useNewUrlParser: true },
+    { 
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    },
     () => console.log('Connected to DB')
 )
 
 // Twiter fetch data :
 Twitter.twitterFetchData();
+
+const job = schedule.scheduleJob('/10 * * * * *', () => {
+    console.log('execute job');
+    let word = 'tpmp';
+
+    T.get('trends/place', { id: 615702 })
+    .catch(function (err) {
+        console.log('caught error', err.stack);
+    })
+    .then(function (result) {
+        // console.log(JSON.stringify(result.data, null, 2));
+        // insérer ou mettre à jour tous les mots clés tendance (50)
+        // Attention, l'api retourne parfois un tweet_volume null, ignorer
+
+        for (let keyword of result.data[0].trends) {
+            console.log("keyword : " + keyword.name + " tweets : " + keyword.tweet_volume);
+        }
+
+    }); 
+});
 
 // Create app Express :
 const app = express()
--- functions/twitterFunctions.js
+++ functions/twitterFunctions.js
@@ -5,7 +5,7 @@ const TwitterFunctions = {
         let count = 2;
         let params = {q: 'node.js', count};
         accesTwiter.get('search/tweets',params, function(error, data, response) {
-            if(error) return console.log("Erreur : L'API Twitter n'arrive pas à vous authentifier.")
+            if(error) return console.log("Erreur : ", error)
             if(response.statusCode != 200) return console.log("Erreur de connexion à l'API Twitter.");
     
             console.log(data.statuses);
