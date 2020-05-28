188c5fade2e75d7ec21a1cd6d19cdeea5f4721a0
TimmyKham <tkhamsithideth@gmail.com>
2020-05-28 17:49:55 +0200

    ADD: Keyword Routes

---
+++ routes/keywordRoute.js
@@ -0,0 +1,22 @@
+const router = require('express-promise-router')()
+const passport = require('passport')
+
+const keywordController = require('../controllers/keywordController');
+const passportJWT = passport.authenticate('jwt', { session: false })
+
+// Keyword Global Route
+router.route('/:woeid')
+    .get(keywordController.getKeywordByCountry)
+    
+// Keyword User Routes
+router.route('/all')
+    .get(keywordController.allKeywords)
+
+router.route('/add')
+    .post(passportJWT, keywordController.addKeyword)
+        
+router.route('/:hashtag')
+    // .put(passportJWT, keywordController.updateKeyword)
+    .delete(passportJWT, keywordController.deleteKeyword)
+
+module.exports = router;
