05f2afc89d7782bb2328c150a7e21abae23d6cae
DigiDataTech <brahim.kaddar@gmail.com>
2020-05-28 18:00:07 +0200

    creation du model keyword

---
+++ models/keywordModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+// Create a schema
+const keywordSchema = new Schema({
+    hashTag: {
+        type: String,
+    },
+    date: {
+        type: Date,
+    },
+    counter: {
+        type: Number,
+    }
+})
+
+// Create a model
+const Keyword = mongoose.model('keyword', keywordSchema)
+
+// Export the model
+module.exports = Keyword;
